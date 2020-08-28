2006b46d6c2e03da320b376d75cf8e7541ff4adc
bblasco <bblasco@asapp.com>
2020-08-28 00:08:44 -0300

    Better tests for Multiple Pattern matcher

--- test/MultiPatternMatcher.spec.ts
+++ test/MultiPatternMatcher.spec.ts
@@ -1,10 +1,11 @@
 import {expect} from 'chai'
 import {MultiPatternMatcher} from '../src/MultiPatternMatcher'
 import {SimpleDomainPattern} from '../src/SimpleDomainPattern'
-import { DomainStripper } from '../src/URIStripper'
+import {SimplePathPattern} from '../src/SimplePathPattern'
+import { DomainStripper, PathStripper, ProtocolStripper } from '../src/URIStripper'
 
-describe('MultiDomainMatcher.ts', () => {
-  it('should match through multiple patterns', () => {
+describe('MultiPatternMatcher.ts', () => {
+  it('should match through multiple domain patterns', () => {
     const matcher = new MultiPatternMatcher(new DomainStripper())
 
     const pattern01 = new SimpleDomainPattern('*.auritylab.com')
@@ -22,23 +23,41 @@ describe('MultiDomainMatcher.ts', () => {
     expect(matchResult02.getPattern()).to.equal(pattern02)
   })
 
-  it ('should match single pattern', () => {
-    const matcher = new MultiPatternMatcher(new DomainStripper())
+  it('should match through multiple path patterns', () => {
+    const matcher = new MultiPatternMatcher(new PathStripper())
 
-    const pattern = new SimpleDomainPattern('*.auritylab.com')
-    matcher.registerPattern(pattern)
+    const pattern01 = new SimplePathPattern("private")
+    const pattern02 = new SimplePathPattern('edit*password')
 
-    const matchResult = matcher.match('test.auritylab.com')
-    expect(matchResult.matches()).to.be.true
-    expect(matchResult.getPattern()).to.equal(pattern)
+    matcher.registerPattern(pattern01)
+    matcher.registerPattern(pattern02)
+
+    const matchResult01 = matcher.match('test.auritylab.com/private/path')
+    expect(matchResult01.matches()).to.be.true
+    expect(matchResult01.getPattern()).to.equal(pattern01)
+
+    const matchResult02 = matcher.match('test.test.com/edit-password/uuid')
+    expect(matchResult02.matches()).to.be.true
+    expect(matchResult02.getPattern()).to.equal(pattern02)
   })
 
-  it ('should accept full url input', () => {
-    const matcher = new MultiPatternMatcher(new DomainStripper())
-    matcher.registerPattern(new SimpleDomainPattern('*.auritylab.com'))
+  it('should match through multiple protocol patterns', () => {
+    const matcher = new MultiPatternMatcher(new ProtocolStripper())
 
-    const result = matcher.match('https://www.auritylab.com')
+    const pattern01 = new SimplePathPattern("http*")
+    const pattern02 = new SimplePathPattern('file')
 
-    expect(result.matches()).to.be.true
+    matcher.registerPattern(pattern01)
+    matcher.registerPattern(pattern02)
+
+    const matchResult01 = matcher.match('https://test.auritylab.com/')
+    expect(matchResult01.matches()).to.be.true
+    expect(matchResult01.getPattern()).to.equal(pattern01)
+
+    const matchResult02 = matcher.match('file://Home/Users/my/path')
+    expect(matchResult02.matches()).to.be.true
+    expect(matchResult02.getPattern()).to.equal(pattern02)
   })
+
+
 })
